c9d297282a32ad1ad3e47166ca8a78776525f3a3
Jonny Reeves <jonny@improbable.io>
2017-02-16 09:23:42 +0000

    js refactor

--- jsplayground/src/example/greeter.js
+++
@@ -1,3 +0,0 @@
-export function sayHi(name) {
-	return `Hello, ${name}`;
-}
--- jsplayground/src/index.js
+++ jsplayground/src/index.js
@@ -1,7 +1,25 @@
-import { sayHi } from './example/greeter.js'
+var protobufjs = require('protobufjs');
+
+/*
+var testProto = require('raw!../../cmd/testproto/test.proto');
+var nestedProto = require('raw!../../cmd/testproto/nested/somenested.proto');
+
+var nestedBuilder = protobufjs.loadProto(nestedProto, "./nested/somenested.proto");
+var builder = protobufjs.loadProto(testProto, nestedBuilder, "./test.proto");
+*/
+
+var testProto = require('./test.proto.json');
+var builder = protobufjs.loadJson(testProto, './test.proto');
+
+console.dir(builder);
+
+
 //import testProto from './code_gen/test.js'
+
+/*
 import testproto from './proto/test_pb.js'
 import { frameMessage, dispatchMessage } from './grpc_browser.js'
+import { TestServiceClient } from './proto/test_grpc_pb.js';
 
 var messages = require('./proto/test_pb');
 
@@ -13,34 +31,11 @@ var req = new proto.mwitkow.testproto.PingRequest()
 req.setValue('hello')
 req.setErrorCodeReturned(2)
 
-function pump(reader, state) {
-  return reader.read()
-    .then(result => {
-      if (result.done) {
-        console.log("Read EOF")
-        return
-      }
-      var to = new Uint16Array(result.value)
-      var out2 = new Uint8Array(to)
-      var count = dispatchMessage(out2.buffer, state)
-      console.log("Found " + count + " messages ")
-      return pump(reader, state);
-    });
-}
 
-window.fetch(
-  'https://localhost:9090/mwitkow.testproto.TestService/PingList',
-  {
-    method: 'post',
-    headers: new Headers({"Content-Type": "application/grpc", "Grpc-Browser-Compat": "true"}),
-    body: frameMessage(req),
-    cache: 'none',
-  }
-).then(function (response) {
-  console.log("Got response, starting to pump")
-  pump(response.body.getReader(), {position: 0, buffer: null})
-}).catch(function (error) {
-  console.log("error " + error)
-})
-
-container.innerText = sayHi("Michal!2")
+var client = new TestServiceClient('https://localhost:9090');
+client.ping(req, function(err, resp) {
+	console.log("ping complete")
+  console.dir(err);
+  console.dir(resp);
+});
+*/
