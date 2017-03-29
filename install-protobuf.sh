690d677e793655d587f4266d3ebdfc56c493d704
Marcus Longmuir <marcuslongmuir@me.com>
2017-03-29 22:19:58 +0100

    Install protobuf

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # This is intended to be run by Travis CI
 
-curl -sL https://github.com/google/protobuf/releases/download/v3.2.0/protobuf-cpp-3.2.0.tar.gz | tar zx
-cd protobuf-cpp-3.2.0
-./configure --prefix=/home/travis && make -j2 && make install
-export PATH=/home/travis/bin:$PATH
+set -ex
+
+curl -sL https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip | unzip
+ls
+pwd
+export PATH=/home/travis/protoc-3.2.0-linux-x86_64/bin:$PATH
+echo $PATH
