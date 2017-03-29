5ae3b1f8547c37566c94fc8da286d69731cf531b
Marcus Longmuir <marcuslongmuir@me.com>
2017-03-29 22:38:42 +0100

    Debugging protobuf

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -3,8 +3,12 @@
 
 set -ex
 
-curl -sL https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip | unzip
+wget https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip
+unzip protoc-3.2.0-linux-x86_64.zip -d protobuf
+echo "ls root"
 ls
+echo "ls protobuf"
+ls protobuf
 pwd
-export PATH=/home/travis/protoc-3.2.0-linux-x86_64/bin:$PATH
+export PATH=/home/travis/gopath/src/github.com/improbable-eng/grpc-web/protobuf/bin:$PATH
 echo $PATH
