83c95e9ec5b9ecc452b802e21f4e9e7d456ea67c
Marcus Longmuir <marcuslongmuir@me.com>
2017-03-30 22:23:21 +0100

    Setting PATH in travis.yml

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -5,5 +5,9 @@ set -ex
 
 wget https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip
 unzip protoc-3.2.0-linux-x86_64.zip -d protobuf
-mv ./protobuf/bin/protoc /usr/local/bin/
-ls /usr/local/bin/
+echo "ls root"
+ls
+echo "ls protobuf"
+ls protobuf
+pwd
+echo $PATH
