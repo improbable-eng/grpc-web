f422eedf3ad695f8f8e478e95b0622901ff69235
Marcus Longmuir <marcuslongmuir@me.com>
2017-03-30 22:31:46 +0100

    Removed debugging

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -2,12 +2,5 @@
 # This is intended to be run by Travis CI
 
 set -ex
-
 wget https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip
 unzip protoc-3.2.0-linux-x86_64.zip -d protobuf
-echo "ls root"
-ls
-echo "ls protobuf"
-ls protobuf
-pwd
-echo $PATH
