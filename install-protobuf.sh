1130154c699fdf79eaf3a4d5455f0ffdb935f9a8
Marcus Longmuir <marcuslongmuir@me.com>
2017-03-31 11:26:21 +0100

    Exposing PROTOBUF_VER as env

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -2,5 +2,5 @@
 # This is intended to be run by Travis CI
 
 set -ex
-wget https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip
-unzip protoc-3.2.0-linux-x86_64.zip -d protobuf
+wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VER/protoc-$PROTOBUF_VER-linux-x86_64.zip
+unzip protoc-$PROTOBUF_VER-linux-x86_64.zip -d protobuf
