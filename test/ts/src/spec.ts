3e1ece2751fca49297ff0a8d3cf36e3a4118a92d
Marcus Longmuir <marcuslongmuir@me.com>
2018-03-06 09:46:31 +0000

    Split out websocket tests

--- test/ts/src/spec.ts
+++ test/ts/src/spec.ts
@@ -1,6 +1,7 @@
 import "./client.spec";
 import "./invoke.spec";
 import "./unary.spec";
+import "./websocket.spec";
 import "./cancellation.spec";
 import "./detach.spec";
 import "./ChunkParser.spec";
