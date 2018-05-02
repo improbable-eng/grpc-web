8705ba3c6ce454e77909c664675a7bc3d02f5357
Marcus Longmuir <marcuslongmuir@me.com>
2018-05-02 16:21:38 +0100

    Updated example to 0.6.2 and updated dependencies

--- example/ts/webpack.config.js
+++ example/ts/webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 module.exports = {
   entry: "./src/index.ts",
+  mode: "development",
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
