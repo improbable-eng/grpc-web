eb5f150d12d248aa47e771abd7f76769659d50f1
Jonah Dahlquist <jonah@limelighthealth.com>
2018-06-08 14:58:43 -0700

    Made transport detection still work in browser integration tests

--- test/ts/src/testRpcCombinations.ts
+++ test/ts/src/testRpcCombinations.ts
@@ -54,8 +54,9 @@ export function runWithHttp1AndHttp2(cb: (config: TestConfig) => void) {
 }
 
 export function runWithSupportedTransports(cb: (transport: grpc.TransportConstructor | undefined) => void) {
+  const default = typeof window === 'undefined' ? nodeHttpRequest : undefined;
   const transports: {[key: string]: grpc.TransportConstructor} = {
-    "defaultTransport": nodeHttpRequest
+    "defaultTransport": default
   };
 
   if (!process.env.DISABLE_WEBSOCKET_TESTS) {
