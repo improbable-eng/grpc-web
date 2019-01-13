5dc0c0be13ce32869d7d3bae9e7e9f19f60cf06c
Jonny Reeves <jonny@improbable.io>
2019-01-13 09:31:52 +0000

    Upgrade ts-protoc-gen to 0.9.0

--- integration_test/ts/_proto/improbable/grpcweb/test/test_pb_service.d.ts
+++ integration_test/ts/_proto/improbable/grpcweb/test/test_pb_service.d.ts
@@ -3,7 +3,7 @@
 
 import * as improbable_grpcweb_test_test_pb from "../../../improbable/grpcweb/test/test_pb";
 import * as google_protobuf_empty_pb from "google-protobuf/google/protobuf/empty_pb";
-import {grpc} from "grpc-web-client";
+import {grpc} from "@improbable-eng/grpc-web";
 
 type TestServicePingEmpty = {
   readonly methodName: string;
@@ -119,96 +119,113 @@ export class FailService {
 
 export type ServiceError = { message: string, code: number; metadata: grpc.Metadata }
 export type Status = { details: string, code: number; metadata: grpc.Metadata }
-export type ServiceClientOptions = { transport: grpc.TransportConstructor; debug?: boolean }
 
+interface UnaryResponse {
+  cancel(): void;
+}
 interface ResponseStream<T> {
   cancel(): void;
   on(type: 'data', handler: (message: T) => void): ResponseStream<T>;
   on(type: 'end', handler: () => void): ResponseStream<T>;
   on(type: 'status', handler: (status: Status) => void): ResponseStream<T>;
 }
+interface RequestStream<T> {
+  write(message: T): RequestStream<T>;
+  end(): void;
+  cancel(): void;
+  on(type: 'end', handler: () => void): RequestStream<T>;
+  on(type: 'status', handler: (status: Status) => void): RequestStream<T>;
+}
+interface BidirectionalStream<ReqT, ResT> {
+  write(message: ReqT): BidirectionalStream<ReqT, ResT>;
+  end(): void;
+  cancel(): void;
+  on(type: 'data', handler: (message: ResT) => void): BidirectionalStream<ReqT, ResT>;
+  on(type: 'end', handler: () => void): BidirectionalStream<ReqT, ResT>;
+  on(type: 'status', handler: (status: Status) => void): BidirectionalStream<ReqT, ResT>;
+}
 
 export class TestServiceClient {
   readonly serviceHost: string;
 
-  constructor(serviceHost: string, options?: ServiceClientOptions);
+  constructor(serviceHost: string, options?: grpc.RpcOptions);
   pingEmpty(
     requestMessage: google_protobuf_empty_pb.Empty,
     metadata: grpc.Metadata,
-    callback: (error: ServiceError, responseMessage: improbable_grpcweb_test_test_pb.PingResponse|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: improbable_grpcweb_test_test_pb.PingResponse|null) => void
+  ): UnaryResponse;
   pingEmpty(
     requestMessage: google_protobuf_empty_pb.Empty,
-    callback: (error: ServiceError, responseMessage: improbable_grpcweb_test_test_pb.PingResponse|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: improbable_grpcweb_test_test_pb.PingResponse|null) => void
+  ): UnaryResponse;
   ping(
     requestMessage: improbable_grpcweb_test_test_pb.PingRequest,
     metadata: grpc.Metadata,
-    callback: (error: ServiceError, responseMessage: improbable_grpcweb_test_test_pb.PingResponse|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: improbable_grpcweb_test_test_pb.PingResponse|null) => void
+  ): UnaryResponse;
   ping(
     requestMessage: improbable_grpcweb_test_test_pb.PingRequest,
-    callback: (error: ServiceError, responseMessage: improbable_grpcweb_test_test_pb.PingResponse|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: improbable_grpcweb_test_test_pb.PingResponse|null) => void
+  ): UnaryResponse;
   pingError(
     requestMessage: improbable_grpcweb_test_test_pb.PingRequest,
     metadata: grpc.Metadata,
-    callback: (error: ServiceError, responseMessage: google_protobuf_empty_pb.Empty|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: google_protobuf_empty_pb.Empty|null) => void
+  ): UnaryResponse;
   pingError(
     requestMessage: improbable_grpcweb_test_test_pb.PingRequest,
-    callback: (error: ServiceError, responseMessage: google_protobuf_empty_pb.Empty|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: google_protobuf_empty_pb.Empty|null) => void
+  ): UnaryResponse;
   pingList(requestMessage: improbable_grpcweb_test_test_pb.PingRequest, metadata?: grpc.Metadata): ResponseStream<improbable_grpcweb_test_test_pb.PingResponse>;
-  pingPongBidi(): void;
-  pingStream(): void;
+  pingPongBidi(metadata?: grpc.Metadata): BidirectionalStream<improbable_grpcweb_test_test_pb.PingRequest, improbable_grpcweb_test_test_pb.PingResponse>;
+  pingStream(metadata?: grpc.Metadata): RequestStream<improbable_grpcweb_test_test_pb.PingRequest>;
   echo(
     requestMessage: improbable_grpcweb_test_test_pb.TextMessage,
     metadata: grpc.Metadata,
-    callback: (error: ServiceError, responseMessage: improbable_grpcweb_test_test_pb.TextMessage|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: improbable_grpcweb_test_test_pb.TextMessage|null) => void
+  ): UnaryResponse;
   echo(
     requestMessage: improbable_grpcweb_test_test_pb.TextMessage,
-    callback: (error: ServiceError, responseMessage: improbable_grpcweb_test_test_pb.TextMessage|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: improbable_grpcweb_test_test_pb.TextMessage|null) => void
+  ): UnaryResponse;
 }
 
 export class TestUtilServiceClient {
   readonly serviceHost: string;
 
-  constructor(serviceHost: string, options?: ServiceClientOptions);
+  constructor(serviceHost: string, options?: grpc.RpcOptions);
   continueStream(
     requestMessage: improbable_grpcweb_test_test_pb.ContinueStreamRequest,
     metadata: grpc.Metadata,
-    callback: (error: ServiceError, responseMessage: google_protobuf_empty_pb.Empty|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: google_protobuf_empty_pb.Empty|null) => void
+  ): UnaryResponse;
   continueStream(
     requestMessage: improbable_grpcweb_test_test_pb.ContinueStreamRequest,
-    callback: (error: ServiceError, responseMessage: google_protobuf_empty_pb.Empty|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: google_protobuf_empty_pb.Empty|null) => void
+  ): UnaryResponse;
   checkStreamClosed(
     requestMessage: improbable_grpcweb_test_test_pb.CheckStreamClosedRequest,
     metadata: grpc.Metadata,
-    callback: (error: ServiceError, responseMessage: improbable_grpcweb_test_test_pb.CheckStreamClosedResponse|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: improbable_grpcweb_test_test_pb.CheckStreamClosedResponse|null) => void
+  ): UnaryResponse;
   checkStreamClosed(
     requestMessage: improbable_grpcweb_test_test_pb.CheckStreamClosedRequest,
-    callback: (error: ServiceError, responseMessage: improbable_grpcweb_test_test_pb.CheckStreamClosedResponse|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: improbable_grpcweb_test_test_pb.CheckStreamClosedResponse|null) => void
+  ): UnaryResponse;
 }
 
 export class FailServiceClient {
   readonly serviceHost: string;
 
-  constructor(serviceHost: string, options?: ServiceClientOptions);
+  constructor(serviceHost: string, options?: grpc.RpcOptions);
   nonExistant(
     requestMessage: improbable_grpcweb_test_test_pb.PingRequest,
     metadata: grpc.Metadata,
-    callback: (error: ServiceError, responseMessage: improbable_grpcweb_test_test_pb.PingResponse|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: improbable_grpcweb_test_test_pb.PingResponse|null) => void
+  ): UnaryResponse;
   nonExistant(
     requestMessage: improbable_grpcweb_test_test_pb.PingRequest,
-    callback: (error: ServiceError, responseMessage: improbable_grpcweb_test_test_pb.PingResponse|null) => void
-  ): void;
+    callback: (error: ServiceError|null, responseMessage: improbable_grpcweb_test_test_pb.PingResponse|null) => void
+  ): UnaryResponse;
 }
 
