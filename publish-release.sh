da054872666e1cabd5079174123a550c6481a2e9
Jonny Reeves <jonny@improbable.io>
2019-05-03 12:09:05 +0100

    Add Release Script (#358)
    * Add Release Script
    
    - Generates grpcwebproxy binaries for win, mac, and linux
    - Uses `github-release` binary to do what it says on the tin and attach the grpcwebproxy binaries to the release
    - Uses `lerna publish` to publish the client packages to npm
    
    * - Add explicit check for github token in env
    - Remove quotes from around bash glob as the github-release script will not expand globs.
    
    * - Inline generate-grpcwebproxy-binaries.sh into publish-release.sh
    - Fix hard-coded version strings in binary filenames
    - Rename "build" folder to "dist"
    
    * - Use `x86_64` suffix for binaries
    
    * - Follow protoc binary filename conventions
    - Zip all binaries
    
    * Remove Travis publish step
    
    We're going to try out github actions instead!

---
+++ publish-release.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "GITHUB_TOKEN not found in environment"
+  exit 1
+fi
+
+TAG=$1
+if [[ -z "$TAG" ]]; then
+  echo "Expected release tag to be passed as the first argument"
+  exit 1
+fi
+
+if [[ ! "$TAG" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Invalid release tag: $TAG"
+  exit 1
+fi
+
+echo "Generating grpcwebproxy binaries"
+cd go
+rm -rf dist
+mkdir -p dist
+
+GOOS=linux GOARCH=amd64 go build -o "dist/grpcwebproxy-$TAG-linux-x86_64" ./grpcwebproxy
+GOOS=windows GOARCH=amd64 go build -o "dist/grpcwebproxy-$TAG-win64.exe" ./grpcwebproxy
+GOOS=darwin GOARCH=amd64 go build -o "dist/grpcwebproxy-$TAG-osx-x86_64" ./grpcwebproxy
+for f in dist/*; do zip -9r "$f.zip" "$f"; done
+ls -l ./dist
+cd ..
+
+echo "Publishing $TAG"
+
+# Create github release and attach server binaries
+./node_modules/.bin/github-release upload \
+  --owner improbable-eng \
+  --repo grpc-web \
+  --tag "$TAG" \
+  --name "$TAG" \
+  --body "See [CHANGELOG](https://github.com/improbable-eng/grpc-web/blob/master/CHANGELOG.md) for details" \
+  go/dist/*.zip
+
+# Publish client modules to NPM.
+npx lerna publish $TAG --yes
