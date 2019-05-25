cbf5301ac2cb185cc5c907db509830c3bba0d141
Filipe Azevedo <filipe@luno.com>
2019-05-25 13:16:19 +0100

    add non root resource test

--- go/grpcweb/wrapper_test.go
+++ go/grpcweb/wrapper_test.go
@@ -15,6 +15,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/http/httptest"
 	"net/textproto"
 	"os"
 	"strconv"
@@ -53,10 +54,11 @@ const (
 
 type GrpcWebWrapperTestSuite struct {
 	suite.Suite
-	httpMajorVersion int
-	listener         net.Listener
-	grpcServer       *grpc.Server
-	wrappedServer    *grpcweb.WrappedGrpcServer
+	httpMajorVersion     int
+	listener             net.Listener
+	grpcServer           *grpc.Server
+	wrappedServer        *grpcweb.WrappedGrpcServer
+	allowNonRootResource bool
 }
 
 func TestHttp2GrpcWebWrapperTestSuite(t *testing.T) {
@@ -65,6 +67,27 @@ func TestHttp2GrpcWebWrapperTestSuite(t *testing.T) {
 
 func TestHttp1GrpcWebWrapperTestSuite(t *testing.T) {
 	suite.Run(t, &GrpcWebWrapperTestSuite{httpMajorVersion: 1})
+}
+
+func TestNonRootResource(t *testing.T) {
+	grpcServer := grpc.NewServer()
+	testproto.RegisterTestServiceServer(grpcServer, &testServiceImpl{})
+	wrappedServer := grpcweb.WrapServer(grpcServer,
+		grpcweb.WithAllowNonRootResource(true),
+		grpcweb.WithOriginFunc(func(origin string) bool {
+			return true
+		}))
+
+	headers := http.Header{}
+	headers.Add("Access-Control-Request-Method", "POST")
+	headers.Add("Access-Control-Request-Headers", "origin, x-something-custom, x-grpc-web, accept")
+	req := httptest.NewRequest("OPTIONS", "http://host/grpc/improbable.grpcweb.test.TestService/Echo", nil)
+	req.Header = headers
+	resp := httptest.NewRecorder()
+	wrappedServer.ServeHTTP(resp, req)
+
+	log.Println(resp.Body)
+	assert.Equal(t, http.StatusOK, resp.Code)
 }
 
 func (s *GrpcWebWrapperTestSuite) SetupTest() {
