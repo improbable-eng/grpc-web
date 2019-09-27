ff665160bfb134daf436152dc4b7f8160870dbb7
Alexander Vasiljev <a.a.vasiljev+git@gmail.com>
2019-09-27 14:48:18 +0700

    Support header whitelist in websocket transport (#558)
    * fix test warning
    
    * A flag for grpcWebProxy to allow to propagate some request headers
    
    The commit also fix grpcweb.WrappedGrpcServer header propagation to gRPC
    in case of using websockets
    
    Closes #548

--- integration_test/go/testserver/testserver.go
+++ integration_test/go/testserver/testserver.go
@@ -165,11 +165,11 @@ func (s *testSrv) PingError(ctx context.Context, ping *testproto.PingRequest) (*
 	if ping.GetSendTrailers() {
 		grpc.SetTrailer(ctx, metadata.Pairs("TrailerTestKey1", "ServerValue1", "TrailerTestKey2", "ServerValue2"))
 	}
+	var msg = "💣"
 	if ping.FailureType == testproto.PingRequest_CODE {
-		return nil, grpc.Errorf(codes.Code(ping.ErrorCodeReturned), "Intentionally returning error for PingError")
-	} else {
-		return nil, grpc.Errorf(codes.Code(ping.ErrorCodeReturned), "💣")
+		msg = "Intentionally returning error for PingError"
 	}
+	return nil, grpc.Errorf(codes.Code(ping.ErrorCodeReturned), msg)
 }
 
 func (s *testSrv) ContinueStream(ctx context.Context, req *testproto.ContinueStreamRequest) (*google_protobuf.Empty, error) {
