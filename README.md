48c514d6ca532d5421f3626710ac285dda8d398f
Marcus Longmuir <marcuslongmuir@me.com>
2020-09-15 13:40:21 +0100

    Update tested browser compatibility

--- README.md
+++ README.md
@@ -138,9 +138,9 @@ The gRPC semantics encourage you to make multiple requests at once. With most mo
 This library is tested against:
   * Chrome >= 41
   * Firefox >= 21
-  * Edge >= 13
+  * Edge >= 16
   * IE >= 11
-  * Safari >= 6
+  * Safari >= 8
   
 ## Node.js Support
 
--- integration_test/browsers.ts
+++ integration_test/browsers.ts
@@ -49,24 +49,35 @@ function browser(browserName, browserVersion, os, osVersion) {
 // Browser versions that should not have any Fetch/XHR differences in functionality to other (tested) versions are
 // commented out.
 const browsers = {
-  edge15_win: browser("edge", "15", "Windows", "10"),
-  edge14_win: browser("edge", "14", "Windows", "10"),
-  edge13_win: browser('edge', "13", 'Windows', "10"),
-  ie11_win: browser('ie', "11", 'Windows', "7"),
-  firefox53_osx: browser('firefox', "53", 'OS X', "Sierra"),
+  // Firefox
+  firefox80_osx: browser('firefox', "80", 'OS X', "Sierra"),
   firefox39_osx: browser('firefox', "39", 'OS X', "Sierra"), // Basic fetch added in 39
   firefox38_osx: browser('firefox', "38", 'OS X', "Sierra"),
   firefox21_osx: browser('firefox', "21", 'OS X', "Sierra"),
-  chrome_57: browser('chrome', "57", 'Windows', "7"),
+
+  // Chrome
+  chrome_85: browser('chrome', "57", 'Windows', "7"),
   chrome_52: browser('chrome', "52", 'Windows', "7"),
   chrome_43: browser('chrome', "43", 'Windows', "7"), // Readable stream fetch support added in 43
   chrome_42: browser('chrome', "42", 'Windows', "7"), // Basic fetch added in 42
   chrome_41: browser('chrome', "41", 'Windows', "7"),
-  safari11: browser("safari", "11", "OS X", "High Sierra"),
-  // // Safari 10 disabled whilst investigating an issue with https requests for unresolved host hanging through Browserstack
-  // safari10: browser("safari", "10", "OS X", "Sierra"),
+
+  // Edge
+  edge85_win: browser("edge", "85", "Windows", "10"),
+  edge18_win: browser("edge", "18", "Windows", "10"),
+  edge17_win: browser("edge", "17", "Windows", "10"),
+  edge16_win: browser('edge', "16", 'Windows', "10"),
+
+  // Safari
+  safari13_1: browser("safari", "13.1", "OS X", "Catalina"),
+  safari12_1: browser("safari", "12.1", "OS X", "Mojave"),
+  safari11_1: browser("safari", "11.1", "OS X", "High Sierra"),
+  safari10_1: browser("safari", "10.1", "OS X", "Sierra"),
   safari9_1: browser("safari", "9.1", "OS X", "El Capitan"),
   safari8: browser("safari", "8", "OS X", "Yosemite"),
+
+  // IE
+  ie11_win: browser('ie', "11", 'Windows', "7"),
 };
 
 export default () => {
