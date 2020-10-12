b96e5ac544b807ad145ef9a694cc6be1e7e53ee2
Marcus Longmuir <marcuslongmuir@me.com>
2020-10-12 15:38:43 +0100

    Removed GLOBAL_TESTSERVER env handling

--- integration_test/run-with-testserver.sh
+++ integration_test/run-with-testserver.sh
@@ -2,19 +2,15 @@
 set -e
 set -x
 
-if [ "$GLOBAL_TESTSERVER" = true ] ; then
-  echo "Not starting testserver as GLOBAL_TESTSERVER is set"
-else
-  function killGoTestServer {
-    echo "Killing testserver..."
-    killall testserver
-  }
+function killGoTestServer {
+  echo "Killing testserver..."
+  killall testserver
+}
 
-  ./start-testserver.sh
+./start-testserver.sh
 
-  # Kill the Go Test server when this script exits or is interrupted.
-  trap killGoTestServer SIGINT
-  trap killGoTestServer EXIT
-fi
+# Kill the Go Test server when this script exits or is interrupted.
+trap killGoTestServer SIGINT
+trap killGoTestServer EXIT
 
 $@
