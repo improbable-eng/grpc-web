8279169f485373488c91d455835b51fd1f71c8fb
Marcus Longmuir <marcuslongmuir@me.com>
2020-12-14 11:16:48 +0000

    GOBIN

--- go/generate-docs.sh
+++ go/generate-docs.sh
@@ -6,7 +6,7 @@ for i in $(find . -iname 'doc.go'); do
     dir="${i%/*}"
     echo "- $dir"
     cd "${dir}"
-    "${GOPATH}/bin/godocdown" -heading=Title -o DOC.md
+    "${GOBIN}/godocdown" -heading=Title -o DOC.md
     ln -s DOC.md README.md 2> /dev/null # can fail
     cd "${oldpwd}"
 done;
