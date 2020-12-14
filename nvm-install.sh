e2a22386a76f8bc19ea4735e15f236e55f65f907
Marcus Longmuir <marcuslongmuir@me.com>
2020-12-14 10:43:01 +0000

    nvm-install config

--- nvm-install.sh
+++ nvm-install.sh
@@ -4,5 +4,3 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.33.5/install.sh | bash
 export NVM_DIR="/home/circleci/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install
-echo 'export NVM_DIR="/opt/circleci/.nvm"' >> $BASH_ENV
-echo "[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"" >> $BASH_ENV
