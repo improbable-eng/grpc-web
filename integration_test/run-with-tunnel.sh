9be595cb29363413194fe4ae54c32c7f3e71ecd5
Marcus Longmuir <marcuslongmuir@me.com>
2020-12-15 11:56:49 +0000

    TravisCI -> CircleCI

---
+++ integration_test/run-with-tunnel.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+cd "$(dirname "$0")"
+
+mkdir -p sauce-connect-proxy
+pushd sauce-connect-proxy
+
+wait_file() {
+  local file="$1"; shift
+  local wait_seconds="${1:-10}"; shift
+  until test $((wait_seconds--)) -eq 0 -o -e "$file" ; do sleep 1; done
+  ((++wait_seconds))
+}
+
+BASE_SAUCELABS_TUNNEL_ID=$(openssl rand -base64 12)
+export SAUCELABS_TUNNEL_ID_NO_SSL_BUMP="$BASE_SAUCELABS_TUNNEL_ID-no-ssl-bump"
+export SAUCELABS_TUNNEL_ID_WITH_SSL_BUMP="$BASE_SAUCELABS_TUNNEL_ID-with-ssl-bump"
+
+
+SAUCELABS_READY_FILE_NO_SSL_BUMP=./sauce-connect-readyfile-no-ssl-bump
+SAUCELABS_READY_FILE_WITH_SSL_BUMP=./sauce-connect-readyfile-with-ssl-bump
+# Clear the ready files in case they already exist
+rm -f $SAUCELABS_READY_FILE_WITH_SSL_BUMP $SAUCELABS_READY_FILE_NO_SSL_BUMP
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  SAUCELABS_TUNNEL_PATH=./sc-4.6.2-linux/bin/sc
+  if [[ ! -f "$SAUCELABS_TUNNEL_PATH" ]]; then
+    wget https://saucelabs.com/downloads/sc-4.6.2-linux.tar.gz
+    tar -xzvf ./sc-4.6.2-linux.tar.gz
+  fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  SAUCELABS_TUNNEL_PATH=./sc-4.6.2-osx/bin/sc
+  if [[ ! -f "$SAUCELABS_TUNNEL_PATH" ]]; then
+    wget https://saucelabs.com/downloads/sc-4.6.2-osx.zip
+    unzip ./sc-4.6.2-osx.zip
+  fi
+else
+  echo "Unsupported platform"
+fi
+
+if [[ -z "${SC_SSL_BUMPING}" ]]; then
+  $SAUCELABS_TUNNEL_PATH \
+    -u $SAUCELABS_USERNAME \
+    -k $SAUCELABS_ACCESS_KEY \
+    --logfile ./saucelabs-no-ssl-bump-logs \
+    --pidfile ./saucelabs-no-ssl-bump-pid \
+    --no-ssl-bump-domains testhost,corshost \
+    --tunnel-identifier $SAUCELABS_TUNNEL_ID_NO_SSL_BUMP \
+    --readyfile $SAUCELABS_READY_FILE_NO_SSL_BUMP \
+    -x https://saucelabs.com/rest/v1 &
+  SAUCELABS_PROCESS_ID_NO_SSL_BUMP=$!
+  echo "SAUCELABS_PROCESS_ID_NO_SSL_BUMP:"
+  echo $SAUCELABS_PROCESS_ID_NO_SSL_BUMP
+fi
+
+if [[ ! -z "${SC_SSL_BUMPING}" ]]; then
+  $SAUCELABS_TUNNEL_PATH \
+    -u $SAUCELABS_USERNAME \
+    -k $SAUCELABS_ACCESS_KEY \
+    --logfile ./saucelabs-with-ssl-bump-logs \
+    --pidfile ./saucelabs-with-ssl-bump-pid \
+    --tunnel-identifier $SAUCELABS_TUNNEL_ID_WITH_SSL_BUMP \
+    --readyfile $SAUCELABS_READY_FILE_WITH_SSL_BUMP \
+    -x https://saucelabs.com/rest/v1 &
+  SAUCELABS_PROCESS_ID_WITH_SSL_BUMP=$!
+  echo "SAUCELABS_PROCESS_ID_WITH_SSL_BUMP:"
+  echo $SAUCELABS_PROCESS_ID_WITH_SSL_BUMP
+fi
+
+function killTunnels {
+  echo "Killing Sauce Labs Tunnels..."
+  if [[ -z "${SC_SSL_BUMPING}" ]]; then
+    kill $SAUCELABS_PROCESS_ID_NO_SSL_BUMP
+  fi
+  if [[ ! -z "${SC_SSL_BUMPING}" ]]; then
+    kill $SAUCELABS_PROCESS_ID_WITH_SSL_BUMP
+  fi
+
+  echo "Printing SauceConnect no-ssl bump logs:"
+  cat ./sauce-connect-proxy/saucelabs-no-ssl-bump-logs || true
+
+  echo "Printing SauceConnect with-ssl bump logs:"
+  cat ./sauce-connect-proxy/saucelabs-with-ssl-bump-logs || true
+}
+
+trap killTunnels SIGINT
+trap killTunnels EXIT
+
+# Wait for tunnels to indicate ready status
+if [[ -z "${SC_SSL_BUMPING}" ]]; then
+  wait_file "$SAUCELABS_READY_FILE_NO_SSL_BUMP" 60 || {
+    echo "Timed out waiting for sauce labs tunnel (with ssl bump)"
+    kill $SAUCELABS_PROCESS_ID_NO_SSL_BUMP
+    exit 1
+  }
+  echo "SAUCELABS_TUNNEL_ID_NO_SSL_BUMP: $SAUCELABS_TUNNEL_ID_NO_SSL_BUMP"
+fi
+
+if [[ ! -z "${SC_SSL_BUMPING}" ]]; then
+  wait_file "$SAUCELABS_READY_FILE_WITH_SSL_BUMP" 60 || {
+    echo "Timed out waiting for sauce labs tunnel (no ssl bump)"
+    kill $SAUCELABS_PROCESS_ID_WITH_SSL_BUMP
+    exit 1
+  }
+  echo "SAUCELABS_TUNNEL_ID_WITH_SSL_BUMP: $SAUCELABS_TUNNEL_ID_WITH_SSL_BUMP"
+fi
+
+popd
+
+$@
--- integration_test/test.sh
+++ integration_test/test.sh
@@ -2,14 +2,17 @@
 set -e
 set +o pipefail
 
-MAX_AUTO_RETRY=5
+if [ -z "$PREBUILT_INTEGRATION_TESTS" ]; then
+  echo "Building integration test JS"
+  npm run build
+else
+  echo "Skipping integration test JS build because PREBUILT_INTEGRATION_TESTS is set"
+fi
 
-npm run build
-
-if [ -z "$BROWSER" ]; then
-  echo "No Browser specified, starting local test run"
+if [ -z "$BROWSER_ONLY" ]; then
+  echo "No Browser only run specified, starting local test run"
   npm run test:node
-  npm run test:browser
+  npm run test:browsers
   exit 0
 fi
 
@@ -18,28 +21,6 @@ if [ "$BROWSER" == "nodejs" ]; then
   exit 0
 fi
 
-# inspired by https://unix.stackexchange.com/a/82602
-retry () {
-    local n=0
-
-    until [ $n -ge $MAX_AUTO_RETRY ]; do
-        set +e
-        "$@" && break
-        set -e
-
-        n=$[$n+1]
-        echo ''
-        echo "Attempt ${n} of ${MAX_AUTO_RETRY} failed, trying again ..."
-        echo ''
-    done
-
-    if [ $n -eq $MAX_AUTO_RETRY ]; then
-        echo "Giving up after ${MAX_AUTO_RETRY} retries"
-        exit 1
-    fi
-}
-
-# Note that browser tests in CI are very flaky, hence the need for the retry wrapper.
-retry npm run test:browser
+npm run test:browsers
 
 exit 0
