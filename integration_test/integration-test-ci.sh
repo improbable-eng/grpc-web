fab73a6e07ffb8acb5847bfe98199cd8f97d6361
Marcus Longmuir <marcuslongmuir@me.com>
2020-12-30 16:17:46 +0000

    Simplified local and CI test scripts

--- integration_test/integration-test-ci.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -x
-
-bash -o pipefail -c "(export PREBUILT_INTEGRATION_TESTS=1 && export BROWSER_ONLY=1 && npm run start) | ts -s \"%.s\""
--- integration_test/test.sh
+++ integration_test/test.sh
@@ -2,25 +2,6 @@
 set -e
 set +o pipefail
 
-if [ -z "$PREBUILT_INTEGRATION_TESTS" ]; then
-  echo "Building integration test JS"
-  npm run build
-else
-  echo "Skipping integration test JS build because PREBUILT_INTEGRATION_TESTS is set"
-fi
-
-if [ -z "$BROWSER_ONLY" ]; then
-  echo "No Browser only run specified, starting local test run"
-  npm run test:node
-  npm run test:browsers
-  exit 0
-fi
-
-if [ "$BROWSER" == "nodejs" ]; then
-  npm run test:node
-  exit 0
-fi
-
+npm run build
+npm run test:node
 npm run test:browsers
-
-exit 0
