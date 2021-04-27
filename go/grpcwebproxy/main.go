6bb1d1e6bfa186e29545b75bd321398d52598163
zolkin <zolkin1@gmail.com>
2021-04-27 14:16:29 +0100

    fix heading go/grpcwebproxy/main.go
    Co-authored-by: Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>

--- go/grpcwebproxy/main.go
+++ go/grpcwebproxy/main.go
@@ -45,7 +45,7 @@ var (
 	flagHttpMaxWriteTimeout = pflag.Duration("server_http_max_write_timeout", 10*time.Second, "HTTP server config, max write duration.")
 	flagHttpMaxReadTimeout  = pflag.Duration("server_http_max_read_timeout", 10*time.Second, "HTTP server config, max read duration.")
 
-	enableRequestDebug = pflag.Bool("enable_request_debug", false, "whether to enable (/debug/requests) and connection(/debug/events) monitoring; also controls prometheus monitoring (/monitoring)")
+	enableRequestDebug = pflag.Bool("enable_request_debug", false, "whether to enable (/debug/requests) and connection(/debug/events) monitoring; also controls prometheus monitoring (/metrics)")
 )
 
 func main() {
